6a325956d3acf8a4fd1502ecdc6114d128d86345
Alice Cai <alicecai@MacBook-Pro-3.local>
2022-06-01 18:38:18 -0700

    Added conversation options

---
+++ webapp/README.md
@@ -0,0 +1,25 @@
+# Chat with GPT-3
+
+This project demonstrates a simple React chat app that lets you chat with GPT-3.
+
+![Chat with GPT-3 in action](https://i.imgur.com/ImzQGG1.gif)
+
+## Running
+
+1. Change `<your-token>` in `/src/services/gpt.ts` to your OpenAI token.
+
+2. 
+``` shell
+npm install
+npm run start
+```
+
+## More
+
+To read more about this project, check out [my guide](zakmiller.com/gpt/project-chat/).
+
+## Credit
+
+Thank you to [Samuel Roth](https://github.com/sejr/react-messenger) for some of the messenger code and [Pubnub](https://laptrinhx.com/add-a-typing-indicator-to-your-react-based-chat-app-1644457033/) for some hints about how to implement the loading indicator in CSS.
+
+And of course thank you OpenAI for GPT-3!
---
+++ webapp/src/components/MessageList/index.tsx
@@ -0,0 +1,107 @@
+import React, { useEffect, useRef, useState } from "react";
+import Compose from "../Compose";
+import Message from "../Message";
+import moment from "moment";
+
+import "./MessageList.css";
+import Toolbar from "../Toolbar";
+import GPTService from "../../services/gpt";
+import MessageBuilderService from "../../services/message-builder";
+import TypingIndicator from "../TypingIndicator";
+
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
+
+const MY_USER_ID = "Patient";
+
+export interface Message {
+  author: string;
+  message: string;
+  timestamp: number;
+}
+export default function MessageList() {
+
+  const options = [
+    'Intellectual Conversation',
+    'Theraputic Conversation',
+    'Reflective Conversation',
+    'Problem-solving Conversation',
+    'Open-ended Conversation'
+  ];
+  const defaultOption = options[0];
+  const [option, setOption] = useState(defaultOption);
+
+  type Conversation = { speaker: string, message: string };
+  let speakerDict = new Map<string, Conversation>();
+  speakerDict.set("Intellectual Conversation", { speaker: "Professor", message: "Hello, I am a professor. Let's talk about something interesting. What is the most fascinating concept you've learned about lately?" });
+  speakerDict.set("Theraputic Conversation", { speaker: "Therapist", message: "Hello, I am a therapist. I am here to help you with your mental health. How do you feel?" });
+  speakerDict.set("Reflective Conversation", { speaker: "Self", message: "Hello, I am you, yourself. I am here to help you reflect on your life. What's new in your life?" });
+  speakerDict.set("Problem-solving Conversation", { speaker: "Counselor", message: "Hello, I am a counselor. I am here to help you solve any problems you may have. What is one thing you're struggling with right now?" });
+  speakerDict.set("Open-ended Conversation", { speaker: "Friend", message: "Hello, I am your friend. I'm here to talk about something fun!" });
+
+
+  const handleOptionChange = (option: string) => {
+    setOption(option);
+    console.log("Option: ", option); setMessages([
+      {
+        author: speakerDict.get(option)!.speaker,
+        message: speakerDict.get(option)!.message,
+        timestamp: new Date().getTime(),
+      },
+    ]);
+    console.log("Conversation info: ", messages)
+  };
+
+  const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      author: speakerDict.get(option)!.speaker,
+      message: speakerDict.get(option)!.message,
+      timestamp: new Date().getTime(),
+    },
+  ] as Message[]);
+
+  // Stays scrolled to bottom.
+  const messagesEndRef = useRef(null);
+  useEffect(() => {
+    if (messagesEndRef) {
+      (messagesEndRef.current as any)?.scrollIntoView({ behavior: "smooth" })
+    }
+  });
+  const addMyMessage = async (value: string) => {
+    const newMessage = {
+      author: MY_USER_ID,
+      message: value,
+      timestamp: new Date().getTime(),
+    };
+    
+    setMessages((previous) => [...previous, newMessage]);
+    setLoading(true);
+    const withMine = [...messages, newMessage];
+    const response = await GPTService.getAIResponse(withMine);
+    const responseMessage = {
+      author: "Therapist",
+      message: response,
+      timestamp: new Date().getTime(),
+    };
+    setMessages((previous) => [...previous, responseMessage]);
+    setLoading(false);
+  };
+
+  return (
+    <div className="message-list">
+      <Toolbar title="Chat with GPT3" />
+      {/* <Dropdown title="Select conversatio type" /> */}
+      <h4>What kind of conversation do you want to have?</h4>
+      <Dropdown options={options} onChange={(e) => handleOptionChange(e.value)} value={defaultOption} placeholder="Select conversation type" />
+
+      <div className="message-list-container">{MessageBuilderService.getMessages(messages, MY_USER_ID)}
+      {loading && <TypingIndicator />}
+      <div ref={messagesEndRef} />
+      </div>
+      
+
+      <Compose addMessage={addMyMessage} />
+    </div>
+  );
+}
